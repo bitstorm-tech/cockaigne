fa02b556f1682ea6f4e650a11a9f27911a0b5b25
Josef Bauer <josef.bauer.1st@gmail.com>
2023-09-09 10:14:06 +0200

    Remove _ from PG env variables

--- internal/persistence/db.go
+++ internal/persistence/db.go
@@ -12,11 +12,11 @@ import (
 var DB *gorm.DB
 
 func ConnectToDb() {
-	pgHost := os.Getenv("PG_HOST")
-	pgPort := os.Getenv("PG_PORT")
-	pgDatabase := os.Getenv("PG_DATABASE")
-	pgUser := os.Getenv("PG_USER")
-	pgPassword := os.Getenv("PG_PASSWORD")
+	pgHost := os.Getenv("PGHOST")
+	pgPort := os.Getenv("PGPORT")
+	pgDatabase := os.Getenv("PGDATABASE")
+	pgUser := os.Getenv("PGUSER")
+	pgPassword := os.Getenv("PGPASSWORD")
 
 	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s", pgHost, pgPort, pgUser, pgDatabase)
 	log.Print("Connecting to database: ", connectionString)
