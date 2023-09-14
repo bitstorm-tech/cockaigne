cd0967fb98f417b69ad1005a8eca9d99bdd89118
Josef Bauer <josef.bauer.1st@gmail.com>
2023-09-15 01:26:19 +0200

    Make category select a partial

--- internal/deal/handler.go
+++ internal/deal/handler.go
@@ -23,4 +23,14 @@ func Register(app *fiber.App) {
 
 		return c.Render("pages/edit-deal", fiber.Map{"deal": deal}, "layouts/main")
 	})
+
+	app.Get("/category-select", func(c *fiber.Ctx) error {
+		categories := []Category{}
+		err := persistence.DB.Find(&categories).Where("active = true").Error
+		if err != nil {
+			return c.Render("partials/alert", fiber.Map{"message": err.Error()})
+		}
+
+		return c.Render("partials/category-select", fiber.Map{"categories": categories})
+	})
 }
