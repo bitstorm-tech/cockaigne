e916cbe1aba8eddd5b13fd17fa7f6d7eb858fd7e
Josef Bauer <josef.bauer.1st@gmail.com>
2023-10-10 10:58:19 +0200

    Add initial filter modal to map page

--- internal/maps/map_handler.go
+++ internal/maps/map_handler.go
@@ -8,4 +8,8 @@ func Register(app *fiber.App) {
 	app.Get("/map", func(c *fiber.Ctx) error {
 		return c.Render("pages/map", nil, "layouts/main")
 	})
+
+	app.Get("/ui/map/filter-modal", func(c *fiber.Ctx) error {
+		return c.Render("partials/modal", nil)
+	})
 }
