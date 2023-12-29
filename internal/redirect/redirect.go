e3d41b13df2a6a9b7cce0f35f643739a2e5775bf
Josef Bauer <josef.bauer.1st@gmail.com>
2023-12-29 21:17:27 +0100

    Some minor admin UI changes

--- internal/redirect/redirect.go
+++ internal/redirect/redirect.go
@@ -1,13 +1,11 @@
 package redirect
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func Login(c echo.Context) error {
-	return c.Redirect(http.StatusTemporaryRedirect, "/login")
+	return To("/login", c)
 }
 
 func To(location string, c echo.Context) error {
