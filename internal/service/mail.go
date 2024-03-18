1391be58af8b0cc106fb936a8317317ff27759d1
Josef Bauer <josef.bauer.1st@gmail.com>
2024-03-18 23:25:59 +0100

    Fix breaking change in Brevo SDK

--- internal/service/mail.go
+++ internal/service/mail.go
@@ -18,11 +18,6 @@ func init() {
 	br = brevo.NewAPIClient(cfg)
 }
 
-type ActivationMailParams struct {
-	ActivationCode int
-	ActivationUrl  string
-}
-
 func SendAccountActivationMail(email string, baseUrl string) error {
 	activationCode := rand.Intn(900000) + 100000
 
@@ -31,45 +26,37 @@ func SendAccountActivationMail(email string, baseUrl string) error {
 		return err
 	}
 
-	params := interface{}(ActivationMailParams{
-		ActivationCode: activationCode,
-		ActivationUrl:  fmt.Sprintf("%s/signup-complete?email=%s&code=%d", baseUrl, email, activationCode),
-	})
+	params := map[string]interface{}{
+		"ActivationCode": activationCode,
+		"ActivationUrl":  fmt.Sprintf("%s/signup-complete?email=%s&code=%d", baseUrl, email, activationCode),
+	}
 
 	templateId := os.Getenv("BREVO_ACTIVATE_ACCOUNT_TEMPLATE_ID")
 
-	return sendMail(templateId, email, &params)
-}
-
-type PasswordChangeParams struct {
-	ChangePasswordUrl string
+	return sendMail(templateId, email, params)
 }
 
 func SendPasswordChangeEmail(email string, code string, baseUrl string) error {
 	templateId := os.Getenv("BREVO_CHANGE_PASSWORD_TEMPLATE_ID")
 
-	params := interface{}(PasswordChangeParams{
-		ChangePasswordUrl: fmt.Sprintf("%s/password-change/%s", baseUrl, code),
-	})
+	params := map[string]interface{}{
+		"ChangePasswordUrl": fmt.Sprintf("%s/password-change/%s", baseUrl, code),
+	}
 
-	return sendMail(templateId, email, &params)
-}
-
-type EmailChangeParams struct {
-	ChangeEmailUrl string
+	return sendMail(templateId, email, params)
 }
 
 func SendEmailChangeEmail(email string, code string, baseUrl string) error {
 	templateId := os.Getenv("BREVO_CHANGE_EMAIL_TEMPLATE_ID")
 
-	params := interface{}(EmailChangeParams{
-		ChangeEmailUrl: fmt.Sprintf("%s/email-change/%s", baseUrl, code),
-	})
+	params := map[string]interface{}{
+		"ChangeEmailUrl": fmt.Sprintf("%s/email-change/%s", baseUrl, code),
+	}
 
-	return sendMail(templateId, email, &params)
+	return sendMail(templateId, email, params)
 }
 
-func sendMail(templateId string, email string, params *interface{}) error {
+func sendMail(templateId string, email string, params map[string]interface{}) error {
 	templId, err := strconv.Atoi(templateId)
 	if err != nil {
 		return err
