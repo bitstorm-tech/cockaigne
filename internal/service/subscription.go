e52e9566e5b04434f24e20f8d7e4e85f40a0486c
Josef Bauer <josef.bauer.1st@gmail.com>
2024-04-20 00:23:12 +0200

    Handle no subscription gracefully

--- internal/service/subscription.go
+++ internal/service/subscription.go
@@ -42,11 +42,11 @@ func GetFreeDaysLeftFromSubscription(dealerId string) (int, error) {
 	daysUsed := 0
 	err := persistence.DB.Get(
 		&daysUsed,
-		`select coalesce(sum(duration_in_hours) / 24, 0)
+		`select coalesce(( select sum(duration_in_hours) / 24
 		from active_deals_view 
 		where date_trunc('month', start) = date_trunc('month', now()) 
 		  and date_trunc('year', start) = date_trunc('year', now()) 
-		  and dealer_id = $1`,
+		  and dealer_id = $1), 0)`,
 		dealerId,
 	)
 	if err != nil {
@@ -65,7 +65,7 @@ func GetFreeDaysPerMonthFromSubscription(dealerId string) (int, error) {
 	freeDaysPerMont := 0
 	err := persistence.DB.Get(
 		&freeDaysPerMont,
-		"select free_days_per_month from subscriptions s join plans p on s.plan_id = p.id where state = $1 and account_id = $2",
+		"select coalesce((select free_days_per_month from subscriptions s join plans p on s.plan_id = p.id where state = $1 and account_id = $2), 0)",
 		model.SubActive,
 		dealerId,
 	)
@@ -77,8 +77,17 @@ func GetFreeDaysPerMonthFromSubscription(dealerId string) (int, error) {
 }
 
 func GetSubscriptionPeriodEndDate(dealerId string) (string, error) {
+	hasActiveSubscription, err := HasActiveSubscription(dealerId)
+	if err != nil {
+		return "", err
+	}
+
+	if !hasActiveSubscription {
+		return "", nil
+	}
+
 	var activationDate time.Time
-	err := persistence.DB.Get(
+	err = persistence.DB.Get(
 		&activationDate,
 		"select activated from subscriptions where account_id = $1",
 		dealerId,
